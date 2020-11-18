36c65320c235475949b5c2175fc27465cb32c18a
Ivan Rey De Cardo <ivanreydecardo@gmail.com>
2020-11-19 06:22:23 +0800

    Fix error: No such file or directory

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 SUBLIME_PATH="${HOME}/Library/Application Support/Sublime Text 3"
 
+mkdir -p "${SUBLIME_PATH}"
+
 find "${SUBLIME_PATH}" -maxdepth 1 -mindepth 1 -exec rm -rf {} \;
 
 git clone https://github.com/idecardo/sublime-text-settings.git "${SUBLIME_PATH}/."
